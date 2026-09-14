138ac0cd59c059493affc43f78fab2415db2fc59
Zik1213 <zik1213@outlook.com>
2026-09-14 20:11:33 +0300

    fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./nix);
 
   inputs = {
-    actions-nix.url = "github:nialov/actions.nix";
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
